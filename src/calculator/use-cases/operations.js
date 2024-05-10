43e7fa241ccd74b4dda0f247369bfa37e2c228df
Johnny S. Bermúdez Soria <johnny.sbsoria@gmail.com>
2024-05-10 20:31:39 +0200

    Funcionality calculator done

--- src/calculator/use-cases/operations.js
+++ src/calculator/use-cases/operations.js
@@ -1,13 +1,140 @@
-const numberButtons    = document.querySelectorAll('[data-number]');
-const operatorButtons  = document.querySelectorAll('[data-operation]');
-const equalButton      = document.querySelector('[data-equal]');
-const plusMinuslButton = document.querySelector('[data-plus-minus]');
-const deleteButton     = document.querySelectorAll('[data-delete]');
-const deleteAllButton  = document.querySelectorAll('[data-delete-all]');
-const deleteOneButton  = document.querySelectorAll('[data-delete-one]');
-const prevOpText       = document.querySelectorAll('[data-prev-op]');
-const currentOpText    = document.querySelectorAll('[data-current-op]');
-
 export const operations = () => {
-    
+    // Recuperamos los botones presionados
+    const numberButtons    = document.querySelectorAll('[data-number]');
+    const operationButtons = document.querySelectorAll('[data-operation]');
+
+    const equalButton      = document.querySelector('[data-equal]');
+
+    const plusMinusButton  = document.querySelector('[data-plus-minus]');
+
+    const deleteAllButton  = document.querySelector('[data-delete-all]');
+    const deleteButton     = document.querySelector('[data-delete]');
+    const deleteOneButton  = document.querySelector('[data-delete-one]');
+
+    const prevOpText       = document.querySelector('[data-prev-op]');
+    const currentOpText    = document.querySelector('[data-current-op]');
+
+    // Variables
+    let currentOperation = ''
+    let prevOperation    = '';
+    let operation        = undefined;
+
+    // Funciones de la calculadora
+    const deleteAll = () => {
+        currentOperation = '';
+        prevOperation    = '';
+        operation        = undefined;
+        updateDisplay();
+    }
+
+    const deleteCurrent = () => {
+        currentOperation = '';
+        updateDisplay();
+    }
+
+    const deleteOne = () => {
+        currentOperation = currentOperation.toString().slice(0, -1);
+        updateDisplay();
+    }
+
+    const appendNumber = (number) => {
+        if (number === '.' && currentOpText.innerHTML.includes('.')) return;
+        currentOperation = currentOperation.toString() + number.toString();
+    }
+
+    const chooseOperation = (op) => {
+        if (currentOperation === '') return;
+
+        if (prevOperation !== '') {
+            compute();
+        }
+
+        operation        = op;
+        prevOperation    = currentOperation;
+        currentOperation = '';
+    }
+
+    const compute = () => {
+        let computation;
+        const prev    = parseFloat(prevOperation);
+        const current = parseFloat(currentOperation);
+
+        if (isNaN(prev) || isNaN(current)) return;
+
+        switch (operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '−':
+                computation = prev - current;
+                break;
+            case '×':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+
+        currentOperation = computation;
+        prevOperation    = '';
+        operation        = undefined;
+    }
+
+    const getDisplayNumber = (number) => {
+        const stringNumber  = number.toString();
+
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        
+        let integerDisplay;
+
+        if (isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('es', {maximumFractionDigits: 0})
+        }
+
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
+    }
+
+    const updateDisplay = () => {
+        currentOpText.innerText = getDisplayNumber(currentOperation);
+
+        if (operation != null) {
+            prevOpText.innerText = `${getDisplayNumber(prevOperation)} ${operation}`;
+        } else {
+            prevOpText.innerText = '';
+        }
+    }
+
+    // Eventos
+    deleteAllButton.addEventListener('click', deleteAll);
+    deleteButton.addEventListener('click', deleteCurrent);
+    deleteOneButton.addEventListener('click', deleteOne);
+
+    numberButtons.forEach(number => {
+        number.addEventListener('click', () => {
+            appendNumber(number.innerText);
+            updateDisplay();
+        });
+    });
+
+    operationButtons.forEach(op => {
+        op.addEventListener('click', () => {
+            chooseOperation(op.innerText);
+            updateDisplay();
+        });
+    });
+
+    equalButton.addEventListener('click', () => {
+        compute();
+        updateDisplay();
+    });
 }
